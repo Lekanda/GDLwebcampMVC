ad94a8014eb114570d190804d3f8457582f6637f
lekanda <abernaolao@gmail.com>
2021-06-10 18:27:40 +0200

    Intregar video en Index.

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -1,1 +1,45 @@
-<h1>Desde la vista de paginas/index.php</h1>
+<section class="seccion contenedor">
+    <h2>La mejor Conferencia de diseño web en Español</h2>
+    <p>Praesent rutrum efficitur pharetra. Vivamus scelerisque pretium velit, id tempor turpis pulvinar et. Ut bibendum finibus massa non molestie. Curabitur urna metus, placerat gravida lacus ut, lacinia congue orci. Maecenas luctus mi at ex blandit vehicula. Morbi porttitor tempus euismod.</p>
+</section>
+<!--Seccion de info-->
+
+<section class="programa">
+    <div class="contenedor-video">
+        <video autoplay loop  muted>
+            <source src="/video/video.mp4" type="video/mp4">
+            <source src="/video/video.webm" type="video/webm">
+            <source src="/video/video.ogv" type="video/ogv">
+        </video>
+    </div>
+    <!--Fin contenedor Video-->
+    <div class="contenido-programa">
+        <div class="contenedor">
+            <div class="programa-evento">
+                <h2>Programa del Evento</h2>
+                <nav class="menu-programa">
+                    <a href="#talleres"><i class="fas fa-code"></i>Talleres</a>
+                    <a href="#conferencias"><i class="fas fa-comment"></i>Conferencias</a>
+                    <a href="#seminarios"><i class="fas fa-university"></i>Seminarios</a>
+                </nav>
+
+                <div id="talleres" class="info-curso ocultar clearfix">
+                    <div class="detalle-evento">
+                        <h3>HTML5, CSS3, JavaScript</h3>
+                        <p><i class="fas fa-clock"></i> 16:00</p>
+                        <p><i class="fas fa-calendar"></i> 10 de Dic</p>
+                        <p><i class="fas fa-user"></i> Andres Bernaola Olivares</p>
+                    </div>
+                    <div class="detalle-evento">
+                        <h3>WordPress</h3>
+                        <p><i class="fas fa-clock"></i> 20:00</p>
+                        <p><i class="fas fa-calendar"></i> 10 de Dic</p>
+                        <p><i class="fas fa-user"></i> Andres Bernaola Olivares</p>
+                    </div>
+                    <a href="#" class="button float-right">Ver Todos</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--Seccion de Programa-->
