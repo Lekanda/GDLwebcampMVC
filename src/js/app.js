151d926c253de46247b1bb9e9b20b16620dd4b67
lekanda <abernaolao@gmail.com>
2021-06-21 18:00:40 +0200

    Variables del DOM para JS.

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,28 @@
 document.addEventListener('DOMContentLoaded', function() {
     eventListeners();
 });
+
+
                             
 function eventListeners() {
-    console.log("DOM cargado");
+    // console.log("DOM cargado");
+
+    //*** VARIABLES ***
+    // Campos Usuario
+    const nombre = document.getElementById('nombre');
+    const apellido = document.getElementById('apellido');
+    const email = document.getElementById('email');
+
+    // Campos Pases
+    const pase_dia = document.getElementById('pase_dia');
+    const pase_completo = document.getElementById('pase_completo');
+    const pase_dosdias = document.getElementById('pase_dosdias');
+
+    // Botones y Divs
+    const calcular = document.getElementById('calcular');
+    const errorDiv = document.getElementById('error');
+    const btnRegistro = document.getElementById('btnRegistro');
+    const resultado = document.getElementById('lista-productos');
+
    
 }
